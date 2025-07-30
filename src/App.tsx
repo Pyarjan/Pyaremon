a55bebe8ff1d5bf294f142bd716a9952e01654a8
itspyarjanchettri <pyarjanskillprompt@gmail.com>
2025-07-30 23:48:26 +0545

    floating navbar

---
+++ src/App.tsx
@@ -0,0 +1,129 @@
+const App = () => {
+  return (
+    <div className="bg-black w-full min-h-screen text-white text-justify p-5">
+      <h1 className="text-5xl font-bold text-center p-5">Floating NavBar</h1>
+      <div className="flex justify-between items-center px-10 border sticky top-20 backdrop-blur bg-white/50 rounded-xl p-3 ">
+        <div className="flex justify-center items-center gap-2">
+          <img src="/png.png" alt="logo" className="h-10 w-10" />
+          <span className="text-xl font-bold">Pyaremon</span>
+        </div>
+        <div className="flex justify-between gap-8 items-center">
+          <a className="hover:text-blue-500 text-base font-semibold " href="">
+            Home
+          </a>
+          <a className="hover:text-blue-500 text-base font-semibold " href="">
+            About
+          </a>
+          <a className="hover:text-blue-500 text-base font-semibold " href="">
+            Contact
+          </a>
+          <a className="hover:text-blue-500 text-base font-semibold " href="">
+            Services
+          </a>
+          <a className="hover:text-blue-500 text-base font-semibold " href="">
+            Tech Stack
+          </a>
+        </div>
+        <button className="bg-white text-black px-5 text-lg font-semibold hover:bg-blue-300 py-2 rounded-xl ">
+          Signup
+        </button>
+      </div>
+
+      <div className="mt-5">
+        <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate
+          ea, iusto enim accusantium animi repudiandae. Corrupti, dolorum
+          nostrum quod rem nobis magnam provident incidunt officia eum iste
+          veritatis. Ut, accusamus.
+        </p>
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+          itaque autem quas, praesentium corrupti dolorem ab deserunt error.
+          Ratione quidem nobis blanditiis excepturi sunt ab laboriosam aliquam
+          eius ipsum totam. Mollitia non tenetur doloribus quos deserunt,
+          aliquam eveniet possimus, quia a, adipisci illo. Nesciunt enim dicta
+          exercitationem, natus soluta laudantium itaque, fugiat libero
+          praesentium laboriosam pariatur amet consequatur modi minus! Possimus
+          necessitatibus expedita omnis rem amet consequuntur iste ab odit
+          incidunt voluptatibus est harum dolore aut culpa obcaecati dolores
+          laborum eos distinctio ea, sapiente temporibus velit id eaque? Earum,
+          maxime! Distinctio, iste assumenda. Beatae ullam nobis laborum quam
+          doloribus maiores. Quasi iste necessitatibus quisquam ut eligendi aut
+          quibusdam provident, cumque voluptas, blanditiis sint consequuntur in
+          architecto iusto, aliquam tenetur delectus. Dolores repellat obcaecati
+          similique magni nostrum laudantium vero adipisci. Quidem blanditiis
+          distinctio cumque porro facere animi quisquam impedit ipsum eius eum
+          illum necessitatibus veritatis natus asperiores doloremque, facilis,
+          fuga quaerat.
+        </p>
+         <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+          itaque autem quas, praesentium corrupti dolorem ab deserunt error.
+          Ratione quidem nobis blanditiis excepturi sunt ab laboriosam aliquam
+          eius ipsum totam. Mollitia non tenetur doloribus quos deserunt,
+          aliquam eveniet possimus, quia a, adipisci illo. Nesciunt enim dicta
+          exercitationem, natus soluta laudantium itaque, fugiat libero
+          praesentium laboriosam pariatur amet consequatur modi minus! Possimus
+          necessitatibus expedita omnis rem amet consequuntur iste ab odit
+          incidunt voluptatibus est harum dolore aut culpa obcaecati dolores
+          laborum eos distinctio ea, sapiente temporibus velit id eaque? Earum,
+          maxime! Distinctio, iste assumenda. Beatae ullam nobis laborum quam
+          doloribus maiores. Quasi iste necessitatibus quisquam ut eligendi aut
+          quibusdam provident, cumque voluptas, blanditiis sint consequuntur in
+          architecto iusto, aliquam tenetur delectus. Dolores repellat obcaecati
+          similique magni nostrum laudantium vero adipisci. Quidem blanditiis
+          distinctio cumque porro facere animi quisquam impedit ipsum eius eum
+          illum necessitatibus veritatis natus asperiores doloremque, facilis,
+          fuga quaerat.
+        </p>
+         <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+          itaque autem quas, praesentium corrupti dolorem ab deserunt error.
+          Ratione quidem nobis blanditiis excepturi sunt ab laboriosam aliquam
+          eius ipsum totam. Mollitia non tenetur doloribus quos deserunt,
+          aliquam eveniet possimus, quia a, adipisci illo. Nesciunt enim dicta
+          exercitationem, natus soluta laudantium itaque, fugiat libero
+          praesentium laboriosam pariatur amet consequatur modi minus! Possimus
+          necessitatibus expedita omnis rem amet consequuntur iste ab odit
+          incidunt voluptatibus est harum dolore aut culpa obcaecati dolores
+          laborum eos distinctio ea, sapiente temporibus velit id eaque? Earum,
+          maxime! Distinctio, iste assumenda. Beatae ullam nobis laborum quam
+          doloribus maiores. Quasi iste necessitatibus quisquam ut eligendi aut
+          quibusdam provident, cumque voluptas, blanditiis sint consequuntur in
+          architecto iusto, aliquam tenetur delectus. Dolores repellat obcaecati
+          similique magni nostrum laudantium vero adipisci. Quidem blanditiis
+          distinctio cumque porro facere animi quisquam impedit ipsum eius eum
+          illum necessitatibus veritatis natus asperiores doloremque, facilis,
+          fuga quaerat.
+        </p>
+         <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+          itaque autem quas, praesentium corrupti dolorem ab deserunt error.
+          Ratione quidem nobis blanditiis excepturi sunt ab laboriosam aliquam
+          eius ipsum totam. Mollitia non tenetur doloribus quos deserunt,
+          aliquam eveniet possimus, quia a, adipisci illo. Nesciunt enim dicta
+          exercitationem, natus soluta laudantium itaque, fugiat libero
+          praesentium laboriosam pariatur amet consequatur modi minus! Possimus
+          necessitatibus expedita omnis rem amet consequuntur iste ab odit
+          incidunt voluptatibus est harum dolore aut culpa obcaecati dolores
+          laborum eos distinctio ea, sapiente temporibus velit id eaque? Earum,
+          maxime! Distinctio, iste assumenda. Beatae ullam nobis laborum quam
+          doloribus maiores. Quasi iste necessitatibus quisquam ut eligendi aut
+          quibusdam provident, cumque voluptas, blanditiis sint consequuntur in
+          architecto iusto, aliquam tenetur delectus. Dolores repellat obcaecati
+          similique magni nostrum laudantium vero adipisci. Quidem blanditiis
+          distinctio cumque porro facere animi quisquam impedit ipsum eius eum
+          illum necessitatibus veritatis natus asperiores doloremque, facilis,
+          fuga quaerat.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
